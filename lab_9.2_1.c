14f4ab15c39392b47915f37985db16d2ac590cd7
princebuha <princebuha105@gmail.com>
2023-12-16 09:32:33 +0530

    first commmit

---
+++ lab_9.2_1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+
+main() {
+    int i;
+    int length;
+    char username[100];
+    char password;
+    int latter,dight,special;
+    printf("enter your storng :-");
+    gets(username);
+    if(length < 6){
+    	printf("your password is storng");
+    	
+	
+	 if( special,latter,dight){
+	 	printf("your password is strong");
+		
+	}
+	else if (special,latter){
+		printf("your password is weak");
+	}
+}
+}
+
+
+    
+
